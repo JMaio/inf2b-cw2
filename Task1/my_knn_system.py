af822f2cd9b58ee7300b54cd8e984c67e590b1b6
JMaio <10330349+JMaio@users.noreply.github.com>
2018-04-01 01:38:50 +0100

    create logic for dynamic dataset access

--- Task1/my_knn_system.py
+++ Task1/my_knn_system.py
@@ -1,24 +1,25 @@
-#! /usr/bin/env python
-# A sample template for my_knn_system.py
-
 import numpy as np
 import scipy.io
+import time
 from my_knn_classify import *
-# import my_knn_classify as knnc
-
 from my_confusion import *
 
 # Load the data set
-#   NB: replace <UUN> with your actual UUN.
-filename = "/afs/inf.ed.ac.uk/group/teaching/inf2b/cwk2/d/<UUN>/data.mat";
-data = scipy.io.loadmat(filename);
+filename = "/afs/inf.ed.ac.uk/group/teaching/inf2b/cwk2/d/s1621503/data.mat"
+# use local data set while not connected to afs
+try:
+    data = scipy.io.loadmat(filename)
+    print("loaded data from afs")
+except Exception:
+    data = scipy.io.loadmat("../data.mat")
+    print("loaded data from local")
 
 # Feature vectors: Convert uint8 to double, and divide by 255.
-Xtrn = data['dataset']['train'][0,0]['images'][0,0].astype(dtype=np.float_) /255.0
-Xtst = data['dataset']['test'][0,0]['images'][0,0].astype(dtype=np.float_) /255.0
+Xtrn = data['dataset']['train'][0, 0]['images'][0, 0].astype(dtype=np.float_) / 255.0
+Xtst = data['dataset']['test'][0, 0]['images'][0, 0].astype(dtype=np.float_) / 255.0
 # Labels : convert float64 to integer, and subtract 1 so that class number starts at 0 rather than 1.
-Ctrn = data['dataset']['train'][0,0]['labels'][0,0].astype(dtype=np.int_)-1
-Ctst = data['dataset']['test'][0,0]['labels'][0,0].astype(dtype=np.int_)-1
+Ctrn = data['dataset']['train'][0, 0]['labels'][0, 0].astype(dtype=np.int_) - 1
+Ctst = data['dataset']['test'][0, 0]['labels'][0, 0].astype(dtype=np.int_) - 1
 
 #YourCode - Prepare measuring time
 
