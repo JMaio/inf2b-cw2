2bf3891643a3e5b35dd19bee27edec11cf37280a
JMaio <10330349+JMaio@users.noreply.github.com>
2018-04-02 19:55:40 +0100

    convert to single precision

--- Task1/my_knn_system.py
+++ Task1/my_knn_system.py
@@ -15,9 +15,9 @@ except Exception:
     print("loaded data from local")
 
 
-# Feature vectors: Convert uint8 to double, and divide by 255.
-Xtrn = data['dataset']['train'][0, 0]['images'][0, 0].astype(dtype=np.float_) / 255.0
-Xtst = data['dataset']['test'][0, 0]['images'][0, 0].astype(dtype=np.float_) / 255.0
+# Feature vectors: Convert uint8 to -double- single-precision for speed , and divide by 255.
+Xtrn = data['dataset']['train'][0, 0]['images'][0, 0].astype(dtype=np.float32) / 255.0
+Xtst = data['dataset']['test'][0, 0]['images'][0, 0].astype(dtype=np.float32) / 255.0
 # Labels : convert float64 to integer, and subtract 1 so that class number starts at 0 rather than 1.
 
 Ctrn = data['dataset']['train'][0, 0]['labels'][0, 0].astype(dtype=np.int_) - 1
@@ -40,7 +40,7 @@ for (k, pred) in zip(kb, Cpreds):
     # YourCode - Get confusion matrix and accuracy for each k in kb.
     CM, acc = my_confusion(Ctst, pred)
     # YourCode - Save each confusion matrix.
-    scipy.io.savemat("cm%d.mat" % k, {'cm': CM})
+    scipy.io.savemat("cm%d.mat" % k, {'cm': CM}, oned_as='row')
     # YourCode - Display the required information - k, N, Nerrs, acc for each element of kb
     N = Xtst.shape[0]
     print("k = %2d, N = %d, Nerrs = %4d, acc = %.2f%%" % (k, N, N * (1 - acc), acc*100))
