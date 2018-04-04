8b347cddb6b12a35af7f5fdd2316a05e6a315dff
JMaio <10330349+JMaio@users.noreply.github.com>
2018-04-04 16:58:22 +0100

    copy confusion matrix code to other tasks

--- Task2/my_confusion.py
+++ Task2/my_confusion.py
@@ -5,7 +5,7 @@
 #   We assume that the original labels have been pre-processed so that
 #   class number starts at 0 rather than 1 to meet the NumPy's array indexing
 #   policy. For example, if the number of classes is K, label values are in
-#   [0,K-1] range. (This conversion does not apply to codig wih Matlab)
+#   [0,K-1] range. (This conversion does not apply to coding wih Matlab)
 
 import numpy as np
 
@@ -14,8 +14,22 @@ def my_confusion(Ctrues, Cpreds):
     #   Ctrues : N-by-1 ndarray of ground truth label vector (dtype=np.int_)
     #   Cpreds : N-by-1 ndarray of predicted label vector (dtype=np.int_)
     # Output:
-    #   CM : K-by-K ndarray of confusion matrix, where CM[i,j] is the number of samples whose target is the ith class that was classified as j (dtype=np.int_)
+    #   CM : K-by-K ndarray of confusion matrix, where CM[i,j] is the number of samples whose target is the ith
+    #           class that was classified as j (dtype=np.int_)
     #   acc : accuracy (i.e. correct classification rate) (type=float)
     #
+    # create initial matrix
+    CM = np.zeros((26, 26))
+
+    # iterate over tuples of (prediction, ground truth)
+    for (pred, act) in zip(Cpreds, Ctrues):
+        # add 1 to (row,column) specified by (prediction, ground truth)
+        CM[pred][act] += 1
+
+    # calculate number of correct predictions by going over diagonal,
+    # (where prediction == ground truth )
+    correct = float(np.sum(np.diag(CM)))
+    # calculate accuracy based on correct divided by total
+    acc = correct / Cpreds.shape[0]
 
     return (CM, acc)
