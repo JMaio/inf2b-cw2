cb50cb61ce53b0e858543cea9c71102dcba5c04b
JMaio <10330349+JMaio@users.noreply.github.com>
2018-04-06 00:12:04 +0100

    add checker from sklearn

---
+++ Task2/bnb_check.py
@@ -0,0 +1,8 @@
+from sklearn.naive_bayes import BernoulliNB
+
+
+def bnb_check_classify(Xtrn, Ctrn, Xtst, threshold):
+    clf = BernoulliNB(alpha=0, binarize=threshold)
+    clf.fit(Xtrn, Ctrn)
+
+    return clf.predict(Xtst)
