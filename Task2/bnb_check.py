75b155a49fd1e1ed4698fa50f4ee969839d69ffb
JMaio <10330349+JMaio@users.noreply.github.com>
2018-04-06 23:03:39 +0100

    simplify return

--- Task2/bnb_check.py
+++ Task2/bnb_check.py
@@ -5,4 +5,4 @@ def bnb_check_classify(Xtrn, Ctrn, Xtst, threshold):
     clf = BernoulliNB(alpha=1, binarize=threshold)
     clf.fit(Xtrn, Ctrn.ravel())
 
-    return (clf.feature_log_prob_, clf.predict(Xtst))
+    return clf.predict(Xtst)
