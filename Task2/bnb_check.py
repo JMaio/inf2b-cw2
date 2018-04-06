713002c3eab46310dce7d59751557d3d13afc202
JMaio <10330349+JMaio@users.noreply.github.com>
2018-04-06 14:13:22 +0100

    change alpha to no smoothing

--- Task2/bnb_check.py
+++ Task2/bnb_check.py
@@ -2,7 +2,7 @@ from sklearn.naive_bayes import BernoulliNB
 
 
 def bnb_check_classify(Xtrn, Ctrn, Xtst, threshold):
-    clf = BernoulliNB(alpha=1, binarize=threshold)
     clf.fit(Xtrn, Ctrn)
+    clf = BernoulliNB(alpha=0, binarize=threshold)
 
     return (clf.feature_count_, clf.predict(Xtst))
