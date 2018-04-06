72b9c6899122e0bd2e5b1bae36e941cf5aa909a7
JMaio <10330349+JMaio@users.noreply.github.com>
2018-04-06 23:03:45 +0100

    set smoothing zero

--- Task2/bnb_check.py
+++ Task2/bnb_check.py
@@ -2,7 +2,7 @@ from sklearn.naive_bayes import BernoulliNB
 import numpy as np
 
 def bnb_check_classify(Xtrn, Ctrn, Xtst, threshold):
-    clf = BernoulliNB(alpha=1, binarize=threshold)
+    clf = BernoulliNB(alpha=0, binarize=threshold)
     clf.fit(Xtrn, Ctrn.ravel())
 
     return clf.predict(Xtst)
