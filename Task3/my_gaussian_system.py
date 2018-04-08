5fc979b3f329698420617792eadf325ac33f56bc
JMaio <j.f.maio98@gmail.com>
2018-04-08 22:47:45 +0200

    coherence of my_gaussian_system with others

--- Task3/my_gaussian_system.py
+++ Task3/my_gaussian_system.py
@@ -3,26 +3,39 @@
 
 import numpy as np
 import scipy.io
+import time
+from my_bnb_classify import *
+from my_confusion import *
 
 # Load the data set
-#   NB: replace <UUN> with your actual UUN.
 filename = "/afs/inf.ed.ac.uk/group/teaching/inf2b/cwk2/d/<UUN>/data.mat";
-data = scipy.io.loadmat(filename);
+# use local data set while not connected to afs
+try:
+    data = scipy.io.loadmat(filename)
+    print("loaded data from afs!")
+except Exception:
+    data = scipy.io.loadmat("../data.mat")
+    print("couldn't data from afs! loading local data...")
 
 # Feature vectors: Convert uint8 to double, and divide by 255
-Xtrn = data['dataset']['train'][0,0]['images'][0,0].astype(dtype=np.float_) /255.0
-Xtst = data['dataset']['test'][0,0]['images'][0,0].astype(dtype=np.float_) /255.0
+Xtrn = data['dataset']['train'][0, 0]['images'][0, 0].astype(dtype=np.float_) / 255.0
+Xtst = data['dataset']['test'][0, 0]['images'][0, 0].astype(dtype=np.float_) / 255.0
 # Labels : convert float64 to integer, and subtract 1 so that class number starts at 0 rather than 1.
-Ctrn = data['dataset']['train'][0,0]['labels'][0,0].astype(dtype=np.int_)-1
-Ctst = data['dataset']['test'][0,0]['labels'][0,0].astype(dtype=np.int_)-1
+Ctrn = data['dataset']['train'][0, 0]['labels'][0, 0].astype(dtype=np.int_) - 1
+Ctst = data['dataset']['test'][0, 0]['labels'][0, 0].astype(dtype=np.int_) - 1
 
-#YourCode - Prepare measuring time
-
-# Run classification
 epsilon = 0.01
+
+# Prepare measuring time
+print("starting timer...")
+t0 = time.clock()
+
+# Run Gaussian classification
+print("running my_gaussian_classify...")
 (Cpreds, Ms, Covs) = my_gaussian_classify(Xtrn, Ctrn, Xtst, epsilon)
 
-#YourCode - Measure the user time taken, and display it.
+# Measure the user time taken, and display it.
+print("done! - time elapsed: %.2f seconds" % (time.clock() - t0))
 
 #YourCode - Get a confusion matrix and accuracy
 
