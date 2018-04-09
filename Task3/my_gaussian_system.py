e5e6b8872a5cb89a67a38696746f7e72060d0a12
JMaio <j.f.maio98@gmail.com>
2018-04-09 18:20:14 +0200

    what's with the full stops man

--- Task3/my_gaussian_system.py
+++ Task3/my_gaussian_system.py
@@ -20,7 +20,7 @@ except Exception:
 # Feature vectors: Convert uint8 to double, and divide by 255
 Xtrn = data['dataset']['train'][0, 0]['images'][0, 0].astype(dtype=np.float_) / 255.0
 Xtst = data['dataset']['test'][0, 0]['images'][0, 0].astype(dtype=np.float_) / 255.0
-# Labels : convert float64 to integer, and subtract 1 so that class number starts at 0 rather than 1.
+# Labels : convert float64 to integer, and subtract 1 so that class number starts at 0 rather than 1
 Ctrn = data['dataset']['train'][0, 0]['labels'][0, 0].astype(dtype=np.int_) - 1
 Ctst = data['dataset']['test'][0, 0]['labels'][0, 0].astype(dtype=np.int_) - 1
 
@@ -34,15 +34,15 @@ t0 = time.clock()
 print("running my_gaussian_classify...")
 (Cpreds, Ms, Covs) = my_gaussian_classify(Xtrn, Ctrn, Xtst, epsilon)
 
-# Measure the user time taken, and display it.
+# Measure the user time taken, and display it
 print("done! - time elapsed: %.2f seconds" % (time.clock() - t0))
 
 #YourCode - Get a confusion matrix and accuracy
 
-#YourCode - Save the confusion matrix as "Task3/cm.mat".
+#YourCode - Save the confusion matrix as "Task3/cm.mat"
 
 #YourCode - Save the mean vector and covariance matrix for class 26,
 #           i.e. save Mu(:,25) and Cov(:,:,25) as "Task3/m26.mat" and
-#           "Task3/cov26.mat", respectively.
+#           "Task3/cov26.mat", respectively
 
-#YourCode - Display the required information - N, Nerrs, acc.
+#YourCode - Display the required information - N, Nerrs, acc
