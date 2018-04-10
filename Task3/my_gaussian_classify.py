756920fd28dbcdf74c24456ef9b71e223669f6ff
JMaio <j.f.maio98@gmail.com>
2018-04-11 01:47:28 +0200

    use argmax instead of argsort

--- Task3/my_gaussian_classify.py
+++ Task3/my_gaussian_classify.py
@@ -73,5 +73,5 @@ def my_gaussian_classify(Xtrn, Ctrn, Xtst, epsilon):
         log_pps[:, c] = log_pp.diagonal().ravel()
         # print(log_pps[:, c])
         print("calculated class #%2d" % c)
-    Cpreds = log_pps.argsort(axis=1)[:, -1]
+    Cpreds = log_pps.argmax(axis=1)
     return (Cpreds, Ms, Covs)
