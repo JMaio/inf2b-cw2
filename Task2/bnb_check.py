a46e84c35627e018bd4887728ea9ad1dcb30f4ef
JMaio <j.f.maio98@gmail.com>
2018-04-10 12:26:32 +0200

    remove task2 files to not submit

--- Task2/bnb_check.py
+++
@@ -1,8 +0,0 @@
-from sklearn.naive_bayes import BernoulliNB
-import numpy as np
-
-def bnb_check_classify(Xtrn, Ctrn, Xtst, threshold):
-    clf = BernoulliNB(alpha=0, binarize=threshold)
-    clf.fit(Xtrn, Ctrn.ravel())
-
-    return clf.predict(Xtst)
