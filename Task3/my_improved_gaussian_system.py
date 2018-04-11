cbfd3c107cfcbd9384553f403b42d1fc2a20a7dc
JMaio <j.f.maio98@gmail.com>
2018-04-11 17:48:19 +0200

    setup system for improved gauss

--- Task3/my_improved_gaussian_system.py
+++ Task3/my_improved_gaussian_system.py
@@ -3,25 +3,30 @@
 
 import numpy as np
 import scipy.io
+import time
+from my_gaussian_classify import *
+from my_confusion import *
 
 # Load the data set
-#   NB: replace <UUN> with your actual UUN.
-filename = "/afs/inf.ed.ac.uk/group/teaching/inf2b/cwk2/d/<UUN>/data.mat";
-data = scipy.io.loadmat(filename);
+filename = "/afs/inf.ed.ac.uk/group/teaching/inf2b/cwk2/d/s1621503/data.mat";
+# use local data set while not connected to afs
+try:
+    data = scipy.io.loadmat(filename)
+    print("loaded data from afs!")
+except Exception:
+    data = scipy.io.loadmat("../data.mat")
+    print("couldn't data from afs! loading local data...")
 
 # Feature vectors: Convert uint8 to double, and divide by 255
-Xtrn = data['dataset']['train'][0,0]['images'][0,0].astype(dtype=np.float_) /255.0
-Xtst = data['dataset']['test'][0,0]['images'][0,0].astype(dtype=np.float_) /255.0
-# Labels : convert float64 to integer, and subtract 1 so that class number starts at 0 rather than 1.
-Ctrn = data['dataset']['train'][0,0]['labels'][0,0].astype(dtype=np.int_)-1
-Ctst = data['dataset']['test'][0,0]['labels'][0,0].astype(dtype=np.int_)-1
+Xtrn = data['dataset']['train'][0, 0]['images'][0, 0].astype(dtype=np.float_) / 255.0
+Xtst = data['dataset']['test'][0, 0]['images'][0, 0].astype(dtype=np.float_) / 255.0
+# Labels : convert float64 to integer, and subtract 1 so that class number starts at 0 rather than 1
+Ctrn = data['dataset']['train'][0, 0]['labels'][0, 0].astype(dtype=np.int_) - 1
+Ctst = data['dataset']['test'][0, 0]['labels'][0, 0].astype(dtype=np.int_) - 1
 
-#YourCode - Prepare measuring time
-
-# Run classification
-# epsilon = 0.01
+## Using PCA to improve the gaussian classifier
 # Cpreds = my_gaussian_classify(Xtrn, Ctrn, Xtst, epsilon)
-Cpreds = my_gaussian_classify(Xtrn, Ctrn, Xtst)
+Cpreds = my_improved_gaussian_classify(Xtrn, Ctrn, Xtst)
 
 #YourCode - Measure the user time taken, and display it.
 
