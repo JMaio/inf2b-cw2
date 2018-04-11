ef29d3d2a7bcce10b22bd23e8fa9eb815befa099
JMaio <j.f.maio98@gmail.com>
2018-04-11 23:39:37 +0200

    move this around

--- Task3/my_gaussian_classify.py
+++ Task3/my_gaussian_classify.py
@@ -45,11 +45,11 @@ def my_gaussian_classify(Xtrn, Ctrn, Xtst, epsilon):
     # create empty array to hold each class covariance matrix
     Covs = np.empty((d, d, c_n))
 
-    # start timer
-    time.clock()
-
     # define epsilon as matrix
     epsil = np.identity(d) * epsilon
+
+    # start timer
+    time.clock()
 
     # foreach class
     for c in range(c_n):
