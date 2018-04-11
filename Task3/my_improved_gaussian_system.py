a2a0e2cd0febd4bc4da1df2bc6e0a7dd5ec26837
JMaio <j.f.maio98@gmail.com>
2018-04-11 23:31:57 +0200

    prep system

--- Task3/my_improved_gaussian_system.py
+++ Task3/my_improved_gaussian_system.py
@@ -24,14 +24,25 @@ Xtst = data['dataset']['test'][0, 0]['images'][0, 0].astype(dtype=np.float_) / 2
 Ctrn = data['dataset']['train'][0, 0]['labels'][0, 0].astype(dtype=np.int_) - 1
 Ctst = data['dataset']['test'][0, 0]['labels'][0, 0].astype(dtype=np.int_) - 1
 
+# Prepare measuring time
+print("starting timer...")
+time.clock()
+
 ## Using PCA to improve the gaussian classifier
 # Cpreds = my_gaussian_classify(Xtrn, Ctrn, Xtst, epsilon)
-Cpreds = my_improved_gaussian_classify(Xtrn, Ctrn, Xtst)
+print("running my_improved_gaussian_classify...")
+Cpreds = my_improved_gaussian_classify(Xtrn, Ctrn, Xtst, dims=10)
 
-#YourCode - Measure the user time taken, and display it.
+# Measure the user time taken, and display it
+print("done! - time elapsed: %.2f seconds" % time.clock())
 
-#YourCode - Get a confusion matrix and accuracy
+# Get a confusion matrix and accuracy
+CM, acc = my_confusion(Ctst, Cpreds)
 
-#YourCode - Save the confusion matrix as "Task3/cm_improved.mat".
+#YourCode - Save the confusion matrix as "Task3/cm_improved.mat"
+scipy.io.savemat("cm_improved.mat", {'cm': CM}, oned_as='row')
 
 #YourCode - Display information if any
+N = Xtst.shape[0]
+print("N = %d, Nerrs = %4d, acc = %.2f%%" \
+    % (N, N * (1 - acc), acc*100))
