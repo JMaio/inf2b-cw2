fb5afaba3d78b8150d1747328c650462f63655a5
JMaio <j.f.maio98@gmail.com>
2018-04-11 21:52:12 +0200

    fix error message derp part2

--- Task3/my_improved_gaussian_system.py
+++ Task3/my_improved_gaussian_system.py
@@ -15,7 +15,7 @@ try:
     print("loaded data from afs!")
 except Exception:
     data = scipy.io.loadmat("../data.mat")
-    print("couldn't data from afs! loading local data...")
+    print("couldn't load data from afs! loading local data...")
 
 # Feature vectors: Convert uint8 to double, and divide by 255
 Xtrn = data['dataset']['train'][0, 0]['images'][0, 0].astype(dtype=np.float_) / 255.0
