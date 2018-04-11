a208c125668ab24f72301c4382446179f7ec3650
JMaio <j.f.maio98@gmail.com>
2018-04-11 13:16:42 +0200

    remove clock from beginning

--- Task3/my_gaussian_classify.py
+++ Task3/my_gaussian_classify.py
@@ -42,8 +42,6 @@ def my_gaussian_classify(Xtrn, Ctrn, Xtst, epsilon):
     # create empty array to hold each class covariance matrix
     Covs = np.empty((d, d, 26))
 
-    # start independent timer for covariance calculation
-    t = time.clock()
     # foreach class
     for c in range(26):
         # get mean (mu) for this class
